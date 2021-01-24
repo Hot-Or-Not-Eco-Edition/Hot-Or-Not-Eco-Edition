bb18e36fc5ef9180f28031901dc349368ee6e345
Anjesh Shrestha <anjeshshrestha@gmail.com>
2021-01-24 13:27:19 -0700

    basic photo upload and view

--- hotornotapp/hotornot_app/hotornot_rater/urls.py
+++ hotornotapp/hotornot_app/hotornot_rater/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
 	path('signup/', views.signup, name='signup'),
 	path('login/', views.login, name='login'),
 	path('leaderboard/', views.leaderboard, name='leaderboard'),
+	path('upload/', views.image_upload, name='upload'),
+    path('success/', views.success, name = 'success'),
+	path('photos/', views.photos, name = 'photos'),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
--- hotornotapp/hotornot_app/hotornot_rater/views.py
+++ hotornotapp/hotornot_app/hotornot_rater/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse 
 from .models import *
 from .forms import *
+from django.conf import settings
+import os
 
 
 # Create your views here.
@@ -23,6 +26,27 @@ def login(request):
 	return render(request, 'login.html', {})
 
 def leaderboard(request):
-	db = ld.objects.all().order_by('-score')
+	db = Leaderboard.objects.all().order_by('-score')
 	context = {'data': db}
 	return render(request, 'leaderboard.html', context)
+
+
+# Create your views here. 
+def photos(request): 
+	db = Images.objects.all()
+	content = {"photos": db}
+	return render(request, 'photos.html', content)
+
+def image_upload(request): 
+    if request.method == 'POST': 
+        form = ImagesForm(request.POST, request.FILES)
+        if form.is_valid(): 
+            form.save() 
+            return redirect('success') 
+    else: 
+        form = ImagesForm() 
+    return render(request, 'upload.html', {'form' : form}) 
+  
+  
+def success(request): 
+    return HttpResponse('successfully uploaded') 
