b4a91f807ec23bf55df01edaf8009358e37e3bcf
Anjesh Shrestha <anjeshshrestha@gmail.com>
2021-01-24 11:39:34 -0700

    basic leaderboard

--- hotornotapp/hotornot_app/hotornot_rater/views.py
+++ hotornotapp/hotornot_app/hotornot_rater/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
 	return render(request, 'index.html', {})
@@ -11,4 +12,7 @@ def login(request):
 	return render(request, 'login.html', {})
 
 def leaderboard(request):
-	return render(request, 'leaderboard.html', {})
+	leader = [['Toronto', '500'], ['Montreal', '400'], ['Vancouver', '300'], ['Calgary', '200'], ['Edmonton', '600'], ['Ottawa', '250'], ['Winnipeg', '350'], ['Quebec City', '175'], ['Kitchener', '256'], ['Halifax', '268'], ['Victoria', '412']]
+	leader = sorted(leader, key=lambda x: x[1], reverse=True)
+	context = {'data': leader}
+	return render(request, 'leaderboard.html', context)
