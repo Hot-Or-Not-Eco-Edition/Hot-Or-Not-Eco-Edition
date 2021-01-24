900b479b8315665347686930fdeaf532501f7928
Anjesh Shrestha <anjeshshrestha@gmail.com>
2021-01-24 12:28:12 -0700

    leaderboard model created

--- hotornotapp/hotornot_app/hotornot_app/settings.py
+++ hotornotapp/hotornot_app/hotornot_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import json
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'hotornot_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
